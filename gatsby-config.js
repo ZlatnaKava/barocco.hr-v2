dabbed287703fd54d46ca09aac47f5f2199cffc5
Luka Puharic <lukap2211@gmail.com>
2020-05-22 00:28:48 -0400

    Initial setup: Wordpress, Tailwnind

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: process.env.GATSBY_WP_BASE_URL,
+        protocol: process.env.GATSBY_WP_PROTOCOL,
+        hostingWPCOM: false,
+        useACF: true,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div className="space-y-1 text-2xl hello">Hello world!</div>
+)
